554c8e98eb5e6b62523e1458c827c4914fddb256
Sohail Nadaf <sohailnadaf967@gmail.com>
2024-05-21 16:53:16 +0530

    created the image-slider

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import Accordian from "./components/accordian";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/starrating";
-import ImageSlider from "./components/image-slider";
+import ImageSlider from "./components/image-slider"
 export default function App(){
   return(
     <div className="App">
        {/* Accordian component */}
-      { <Accordian /> }
+      {/* { <Accordian /> } */}
        {/* random_color */}
        {/* <RandomColor /> */}
        {/* start rating
       <StarRating  noOfStars={10}/> */}
       
       {/* image slider component */}
-      {/* <ImageSlider url={"https://picsum.photos/v2/list"}
+      <ImageSlider url={"https://picsum.photos/v2/list"}
         page={"1"}
-        limit={"10"}/> */}
+        limit={"10"}/>
     </div>
   )
 }
--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -1,41 +1,81 @@
 import { useEffect, useState } from "react";
-import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+import { BsArrowRightCircleFill, BsArrowLeftCircleFill } from "react-icons/bs";
 import "./style.css";
 
-export default function ImageSlider({ url, limit = 5, page = 1 }) {
+const ImageSlider = ({ url, page = 1, limit = 10 }) => {
   const [images, setImages] = useState([]);
   const [currentSlide, setCurrentSlide] = useState(0);
-  const [errorMsg, setErrorMsg] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  async function fetchImages(getUrl) {
-    try {
-      setLoading(true);
-
-      const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
-      const data = await response.json();
-
-      if (data) {
-        setImages(data);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(`${url}?page=${page}&limit=${limit}`);
+        const responseData = await response.json();
+        if (responseData) {
+          setImages(responseData);
+          setLoading(false);
+        }
+      } catch (error) {
+        console.error(error);
         setLoading(false);
       }
-    } catch (e) {
-      setErrorMsg(e.message);
-      setLoading(false);
-    }
-  }
-  useEffect(()=>{
-    if(url !=="")fetchImages(url);
-    console.log(images)
-  },[url])
-  
+    };
 
-  if(loading){
-    return<div> Loading Please Wait</div>
+    fetchData();
+  }, [url, page, limit]);
+
+  if (loading) {
+    return <div>Loading...</div>;
   }
-  if(errorMsg !=null){
-    return <div>Error Occured {errorMsg}</div>
-  }
-  
-  return<div className="container"></div>
-}
+
+  const handlePrevious = () => {
+    setCurrentSlide((prevSlide) =>
+      prevSlide === 0 ? images.length - 1 : prevSlide - 1
+    );
+  };
+
+  const handleNext = () => {
+    setCurrentSlide((prevSlide) =>
+      prevSlide === images.length - 1 ? 0 : prevSlide + 1
+    );
+  };
+
+  return (
+    <div className="container">
+      <BsArrowLeftCircleFill
+        onClick={handlePrevious}
+        className="arrow arrow-left"
+      />
+      {images.length > 0 && (
+        <img
+          key={images[currentSlide].id}
+          src={images[currentSlide].download_url}
+          className="currentImg"
+          alt="Slide"
+        />
+      )}
+      <BsArrowRightCircleFill
+        onClick={handleNext}
+        className="arrow arrow-right"
+      />
+      <span className="circle-indicators">
+        {images.length > 0 &&
+          images.map((_, index) => (
+            <button
+              key={index}
+              className={
+                currentSlide === index
+                  ? "current-indicator"
+                  : "inactive-indicator"
+              }
+              onClick={() => setCurrentSlide(index)}
+            ></button>
+          ))}
+      </span>
+    </div>
+  );
+};
+
+export default ImageSlider;
